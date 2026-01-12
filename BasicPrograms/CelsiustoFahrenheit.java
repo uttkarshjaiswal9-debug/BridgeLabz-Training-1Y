469f0644a00a9b812d73a5514b8a68dab8e25517
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-12 20:17:44 +0530

    Added: Basic programs for programming constructs.

--- BasicPrograms/CelsiustoFahrenheit.java
+++ BasicPrograms/CelsiustoFahrenheit.java
@@ -1,16 +1,10 @@
 import java.util.Scanner;
 
-public class CelsiustoFahrenheit{
+class CelsiustoFahrenheit {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter temperature in Celsius: ");
-        double celsius = input.nextDouble();
-
+        Scanner sc = new Scanner(System.in);
+        double celsius = sc.nextDouble();
         double fahrenheit = (celsius * 9 / 5) + 32;
-
-        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
-        
-        input.close();
+        System.out.println(fahrenheit);
     }
 }
