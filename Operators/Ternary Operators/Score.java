76857474de7df3a88ade3807031ece3a3b05ff28
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-20 17:03:10 +0530

    Added: Ternary Operators for workshop.

---
+++ Operators/Ternary Operators/Score.java
@@ -0,0 +1,8 @@
+public class Score {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("Max: " + max);
+    }
+}
