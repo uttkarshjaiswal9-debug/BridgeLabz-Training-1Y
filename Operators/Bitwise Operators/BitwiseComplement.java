fa92da47ef7c1796499c5c3f43c12b9a658ecc47
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-20 16:40:28 +0530

    Added: Bitwise Operators for workshop.

---
+++ Operators/Bitwise Operators/BitwiseComplement.java
@@ -0,0 +1,6 @@
+public class BitwiseComplement {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(~a);
+    }
+}
---
+++ Operators/Bitwise Operators/RightShift.java
@@ -0,0 +1,6 @@
+public class RightShift {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println(a >> 1);
+    }
+}
