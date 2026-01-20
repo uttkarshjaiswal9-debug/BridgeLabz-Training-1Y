46633afc829fcd1735666d8449e7d7de0e735dfd
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-20 16:15:27 +0530

    Added: Relational Operators for workshop.

---
+++ Operators/Relational Operators/GreaterEqual.java
@@ -0,0 +1,7 @@
+public class GreaterEqual {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 50;
+        System.out.println(x >= y);
+    }
+}
---
+++ Operators/Relational Operators/LessEqual.java
@@ -0,0 +1,7 @@
+public class LessEqual {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 25;
+        System.out.println(x <= y);
+    }
+}
