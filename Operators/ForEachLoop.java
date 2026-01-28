d8ff5d160b2b5287f10d31240e500560a473096f
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-28 09:51:56 +0530

    Added: ForEachLoop Operators for workshop.

---
+++ Operators/ForEachLoop.java
@@ -0,0 +1,9 @@
+class ForEachLoop{
+    public static void main(String[] args) {
+        int numbers[] = {10, 20, 30, 40};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
