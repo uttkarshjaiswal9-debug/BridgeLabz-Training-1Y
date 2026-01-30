a8ce8714032e170d6897f20476091c015029600a
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-01-30 14:43:15 +0530

    Added: String Switch example for workshop.

---
+++ Operators/String_Switch.java
@@ -0,0 +1,16 @@
+public class String_Switch{
+	public static void main (String[] args){
+		char c = 'a' ;
+		switch (c){
+			case 'a' :
+				System.out.println("Condition matched and print a");
+				break ;
+			case 'b' :
+				System.out.println("Condition matched and print b");
+				break ; 
+			case 'c' :
+				System.out.println("Condition matched and print c");
+				break ; 
+		}
+	}
+}
