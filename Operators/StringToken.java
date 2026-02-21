e2298702b12a0439b8b525d4517f2641130c1651
uttkarshjaiswal9-debug <uttkarshjaiswal9@gmail.com>
2026-02-21 12:14:28 +0530

    Add string tokenization example

---
+++ Operators/StringToken.java
@@ -0,0 +1,36 @@
+import java.util.StringTokenizer;
+
+public class StringToken {
+    public static void main(String[] args) {
+
+        String str = "Java is easy";
+
+        StringTokenizer st = new StringTokenizer(str);
+
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+}
+public class StringToken {
+   
+   
+    static int addition(int a,int b)
+    {
+      return a+b;
+    }
+
+     int addition(int a,int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        StringToken st = new StringToken()
+
+      int result =st.addition(10,20);
+       System.out.println(result);
+            //  System.out.println(b);
+
+       
+    }
+}
