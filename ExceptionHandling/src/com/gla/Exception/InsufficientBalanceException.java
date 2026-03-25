4b967770f72a215011b57b0fea59e97cb05bafc9
Uttkarsh Jaiswal <uttkarshjaiswal9@gmail.com>
2026-03-25 12:10:23 +0530

    Delete ExceptionHandling/src/com/gla/Exception directory

--- ExceptionHandling/src/com/gla/Exception/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.gla.Exception;
-
-public class InsufficientBalanceException extends Exception {
-    public InsufficientBalanceException(String msg) {
-        super(msg);
-    }
-}
--- ExceptionHandling/src/com/gla/Exception/InvalidAgeException.java
+++
@@ -1,9 +0,0 @@
-package com.gla.Exception;
-
-import java.util.Scanner;
-
-class InvalidAgeException extends Exception {
-    InvalidAgeException(String message) {
-        super(message);
-    }
-}
