da188dd911270b28144473d860b1e0ad2990f425
Uttkarsh Jaiswal <uttkarshjaiswal9@gmail.com>
2026-04-28 14:34:01 +0530

    Delete interfaces-abstraction/src/com/gla/interfacesabstraction directory

--- interfaces-abstraction/src/com/gla/interfacesabstraction/ImplementingInterfaces/payment/CreditCard.java
+++
@@ -1,9 +0,0 @@
-package com.gla.interfacesabstraction.ImplementingInterfaces.payment;
-
-public class CreditCard implements Payment {
-
-    @Override
-    public void pay(double amount) {
-        System.out.println("Paid ₹" + amount + " using Credit Card.");
-    }
-}
